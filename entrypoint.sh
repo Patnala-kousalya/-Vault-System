6b34c6753a0746da969f221d2787e707e3dcdf0f
Patnala-kousalya <199551914+Patnala-kousalya@users.noreply.github.com>
2026-03-11 10:43:03 +0530

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+
+echo "Installing dependencies..."
 npm install
-npm run deploy
+
+echo "Compiling contracts..."
+npx hardhat compile
+
+echo "Deploying vault contract..."
+npx hardhat run scripts/deploy.js
